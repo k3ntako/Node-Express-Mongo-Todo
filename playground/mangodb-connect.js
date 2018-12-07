10db1661ae0cb7fecd46249c2b8fc8fd6a8eb293
Kentaro Kaneki <k@Ken-2.local>
2018-12-06 19:06:06 -0500

    Initial commit

--- playground/mangodb-connect.js
+++ playground/mangodb-connect.js
@@ -1,6 +1,5 @@
 const {MongoClient, ObjectID} = require('mongodb');
-let obj = new ObjectID ();
-console.log(obj);
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   if(err){
     return console.log("Unable to connect to MongoDB server.");
--- playground/mangodb-find.js
+++ playground/mangodb-find.js
@@ -1,5 +1,5 @@
 const {MongoClient, ObjectID} = require('mongodb');
-let obj = new ObjectID ();
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   if(err){
     return console.log("Unable to connect to MongoDB server.");
